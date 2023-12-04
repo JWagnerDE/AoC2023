3825af877d374200cddd9443b706995ec76ac13f
Jannis Wagner <jannis.wagner.de@gmail.com>
2023-12-04 22:36:04 +0100

    Nearly finishes day3 puzzle2.

--- rust/src/bin/day3.rs
+++ rust/src/bin/day3.rs
@@ -3,12 +3,13 @@ use std::env;
 use std::fs;
 
 use std::char;
+use std::cmp;
 
 #[derive(Debug, PartialEq, Eq)]
 enum Entry {
     DOT,
     NUMBER(u8),
-    SYMBOL,
+    SYMBOL(char),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -22,15 +23,17 @@ impl Entry {
                 Some(n) => Ok(Self::NUMBER(n as u8)),
                 None => Err(ParseEntryError),
             },
-            _ => Ok(Self::SYMBOL),
+            c => Ok(Self::SYMBOL(c)),
         }
     }
 }
 
 fn puzzle1(data: &str) -> i32 {
+    let mut sum = 0;
+
     let lines: Vec<&str> = data.lines().collect();
-    let y = lines.len();
-    let x = lines[0].len();
+    let max_x = lines.len() - 1;
+    let max_y = lines[0].len() - 1;
     let map: Vec<Vec<Entry>> = lines
         .iter()
         .map(|line| {
@@ -40,55 +43,125 @@ fn puzzle1(data: &str) -> i32 {
                 .unwrap()
         })
         .collect();
+    // println!("data:\n'{}'", data);
     for (x, line) in map.iter().enumerate() {
+        let mut number: u32 = 0;
+        let mut number_valid = false;
         for (y, entry) in line.iter().enumerate() {
             match entry {
-                Entry::SYMBOL => {
-
-                },
-                _ => {}
+                Entry::NUMBER(d) => {
+                    number = number * 10 + *d as u32;
+                    'windowing: for dx in x.checked_sub(1).unwrap_or(0)..=cmp::min(x + 1, max_x) {
+                        for dy in y.checked_sub(1).unwrap_or(0)..=cmp::min(y + 1, max_y) {
+                            if let Entry::SYMBOL(_) = map[dx][dy] {
+                                number_valid = true;
+                                break 'windowing;
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    if number_valid {
+                        // println!("{:?} -> x:{}, y:{} -> num:{}, sum:{}", entry, x, y, number, sum);
+                        sum += number;
+                    }
+                    number = 0;
+                    number_valid = false;
+                }
             }
         }
+        if number_valid {
+            // println!("End -> x:{} -> num:{}, sum:{}", x, number, sum);
+            sum += number;
+        }
     }
-    // "123*..456"
-    // "123..*456"
-    // "123$.*456"
-    // "123#.*456"
-    // "123...456
-    // ".*......."
-    //
-    // "123...456"
-    // "......$.."
-    //
-    // "123...456"
-    // ".....#..."
-    //
-    // "123...456"
-    // "...$.#..."
-    //
-    // "..34....3"
-    // ".$......."
-    // "123...456"
-    //
-    // "..34....3"
-    // "...*...$."
-    // "123...456"
-    // "...$.#..."
-    //
-    // "..34....3"
-    // "...*....."
-    // "123...456"
-    // "........."
-    //
-    // "..348...."
-    // "...$.*..."
-    // "123...456"
-    // "........."
-    0
+    // println!("END SUM: {}", sum);
+    sum as i32
 }
 
 fn puzzle2(data: &str) -> i32 {
-    0
+    let mut sum = 0;
+
+    let lines: Vec<&str> = data.lines().collect();
+    let max_x = lines.len() - 1;
+    let max_y = lines[0].len() - 1;
+    let map: Vec<Vec<Entry>> = lines
+        .iter()
+        .map(|line| {
+            line.chars()
+                .map(Entry::from_char)
+                .collect::<Result<Vec<Entry>, ParseEntryError>>()
+                .unwrap()
+        })
+        .collect();
+    // println!("data:\n{}", data);
+    for (y, line) in map.iter().enumerate() {
+        for (x, entry) in line.iter().enumerate() {
+            // println!("Testing {},{}", x,y);
+            match entry {
+                Entry::SYMBOL('*') => {
+                    let mut numbers  = Vec::new();
+                    // println!("Is *. Checking window");
+                    for dy in y.checked_sub(1).unwrap_or(0)..=cmp::min(y + 1, max_y) {
+                        let mut x_end = 0;
+                        for dx in x.checked_sub(1).unwrap_or(0)..=cmp::min(x + 1, max_x) {
+                            // println!("win: {},{}", dx, dy);
+                            if x_end > dx {
+                                // println!("continue: {} > {}", x_end, dx);
+                                continue;
+                            }
+                            if let Entry::NUMBER(_) = map[dy][dx] {
+                                let mut x_start = dx;
+                                // println!("Is number. Searching for start starting from x: {}", x_start);
+                                while x_start > 0 {
+                                    x_start -= 1;
+                                    match map[dy][x_start] {
+                                        Entry::NUMBER(_) => {},
+                                        _ => {
+                                            x_start += 1;
+                                            break;
+                                        }
+                                    }
+                                }
+                                // println!("x_start: {}", x_start);
+                                // println!("Evaluating number");
+                                x_end = x_start;
+                                let mut num: u32 = 0;
+                                while x_end <= max_x {
+                                    // println!("checking number at {}, {}", x_end, dy);
+                                    match map[dy][x_end] {
+                                        Entry::NUMBER(d) => {
+                                            num = num*10 + d as u32;
+                                            // println!("d: {}, num: {}", d, num);
+                                        }
+                                        _ => {
+                                            numbers.push(num.clone());
+                                            // println!("Number done at x {}. Num is {}. Numbers are {:?}", x_end, num, numbers);
+                                            break;
+                                        }
+                                    }
+                                    x_end += 1;
+                                }
+                            }
+                        }
+                    }
+                    if numbers.len() > 1 {
+                        println!("{:?}", numbers);
+                        let number = numbers
+                            .iter()
+                            .copied()
+                            .reduce(|acc, e| acc * e)
+                            .unwrap();
+                        // println!("number before sum {}. Sum {}", number, sum);
+                        sum += number;
+                    }
+                }
+                _ => {},
+            }
+        }
+    }
+    // println!("END SUM: {}", sum);
+    sum as i32
 }
 
 fn main() {
@@ -134,15 +207,15 @@ mod tests {
             348 + 123 + 456
         );
         let test_data = "\
-                         467..114..
-                         ...*......
-                         ..35..633.
-                         ......#...
-                         617*......
-                         .....+.58.
-                         ..592.....
-                         ......755.
-                         ...$.*....
+                         467..114..\n\
+                         ...*......\n\
+                         ..35..633.\n\
+                         ......#...\n\
+                         617*......\n\
+                         .....+.58.\n\
+                         ..592.....\n\
+                         ......755.\n\
+                         ...$.*....\n\
                          .664.598..";
         let res = puzzle1(test_data);
         assert_eq!(res, 4361);
@@ -150,17 +223,18 @@ mod tests {
 
     #[test]
     fn test_puzzle2() {
-        assert_eq!(
-            puzzle2("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
-            48
-        );
         let test_data = "\
-                         Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
-                         Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
-                         Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
-                         Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
-                         Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+                         467..114..\n\
+                         ...*......\n\
+                         ..35..633.\n\
+                         ......#...\n\
+                         617*......\n\
+                         .....+.58.\n\
+                         ..592.....\n\
+                         ......755.\n\
+                         ...$.*....\n\
+                         .664.598..";
         let res = puzzle2(test_data);
-        assert_eq!(res, 2286);
+        assert_eq!(res, 467835);
     }
 }
