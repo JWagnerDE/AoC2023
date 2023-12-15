cd1852e643a368d2cfbb52284743c21b31e82dc6
Jannis Wagner <jannis.wagner@microdrones.com>
2023-12-15 09:06:48 +0100

    Finished day7

--- rust/src/bin/day7.rs
+++ rust/src/bin/day7.rs
@@ -4,50 +4,106 @@ use std::fs;
 
 use std::cmp::Ordering;
 use std::collections::HashMap;
-use std::str::FromStr;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialOrd, Ord, PartialEq, Eq)]
 enum Hand {
-    FiveOfAKind,
-    FourOfAKind,
-    FullHouse,
-    ThreeOfAKind,
-    TwoPair,
-    OnePair,
     HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct ParseHandError;
 
-impl FromStr for Hand {
-    type Err = ParseHandError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Hand {
+    fn from_str(s: &str, with_joker: bool) -> Result<Self, ParseHandError> {
+        if s.len() != 5 {
+            return Err(ParseHandError);
+        }
         let mut m = HashMap::new();
         for c in s.chars() {
             let count = m.entry(c).or_insert(0);
             *count += 1;
         }
+        let joker_count = if with_joker {
+            *m.get(&'J').unwrap_or(&0)
+        } else {
+            0
+        };
         match m.len() {
             1 => Ok(Self::FiveOfAKind),
             2 => match m.values().max().ok_or(ParseGameError) {
                 Ok(amount) => match amount {
-                    4 => Ok(Self::FourOfAKind),
-                    3 => Ok(Self::FullHouse),
+                    4 => match joker_count {
+                        0 => Ok(Self::FourOfAKind),
+                        1 | 4 => Ok(Self::FiveOfAKind),
+                        _ => unreachable!(),
+                    },
+                    3 => match joker_count {
+                        0 => Ok(Self::FullHouse),
+                        2 | 3 => Ok(Self::FiveOfAKind),
+                        _ => unreachable!(),
+                    },
                     _ => unreachable!(),
                 },
-                Err(_) => Err(ParseHandError),
+                Err(_) => unreachable!(),
             },
             3 => match m.values().max().ok_or(ParseHandError)? {
-                3 => Ok(Self::ThreeOfAKind),
-                2 => Ok(Self::TwoPair),
-                _ => Err(ParseHandError),
+                3 => match joker_count {
+                    0 => Ok(Self::ThreeOfAKind),
+                    1 | 3 => Ok(Self::FourOfAKind),
+                    _ => unreachable!(),
+                },
+                2 => match joker_count {
+                    0 => Ok(Self::TwoPair),
+                    1 => Ok(Self::FullHouse),
+                    2 => Ok(Self::FourOfAKind),
+                    _ => unreachable!(),
+                },
+                _ => unreachable!(),
             },
-            4 => Ok(Self::OnePair),
-            5 => Ok(Self::HighCard),
-            _ => Err(ParseHandError),
+            4 => match joker_count {
+                0 => Ok(Self::OnePair),
+                1 | 2 => Ok(Self::ThreeOfAKind),
+                _ => unreachable!(),
+            },
+            5 => match joker_count {
+                0 => Ok(Self::HighCard),
+                1 => Ok(Self::OnePair),
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
         }
+    }
+}
+
+fn card_value(c: &char, with_joker: bool) -> Option<u8> {
+    match c.to_digit(10) {
+        Some(d) => {
+            if d >= 2 {
+                Some(d as u8)
+            } else {
+                None
+            }
+        }
+        None => match c {
+            'T' => Some(10),
+            'J' => {
+                if with_joker {
+                    Some(1)
+                } else {
+                    Some(11)
+                }
+            }
+            'Q' => Some(12),
+            'K' => Some(13),
+            'A' => Some(14),
+            _ => None,
+        },
     }
 }
 
@@ -56,22 +112,22 @@ struct Game {
     hand: Hand,
     hand_str: String,
     bid: u32,
+    with_joker: bool,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct ParseGameError;
 
-impl FromStr for Game {
-    type Err = ParseGameError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Game {
+    fn from_str(s: &str, with_joker: bool) -> Result<Self, ParseGameError> {
         let (hand_str, bid_str) = s.split_once(" ").ok_or(ParseGameError)?;
         let bid = bid_str.parse().map_err(|_| ParseGameError)?;
-        let hand = Hand::from_str(hand_str).map_err(|_| ParseGameError)?;
+        let hand = Hand::from_str(hand_str, with_joker).map_err(|_| ParseGameError)?;
         Ok(Self {
             hand,
             hand_str: hand_str.to_owned(),
             bid,
+            with_joker,
         })
     }
 }
@@ -84,27 +140,60 @@ impl PartialOrd for Game {
 
 impl Ord for Game {
     fn cmp(&self, other: &Self) -> Ordering {
-        todo!()
+        match self.hand.cmp(&other.hand) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Equal => {
+                let zipped = Iterator::zip(self.hand_str.chars(), other.hand_str.chars());
+                for (s, o) in zipped {
+                    let s_val = card_value(&s, self.with_joker).unwrap();
+                    let o_val = card_value(&o, self.with_joker).unwrap();
+                    match s_val.cmp(&o_val) {
+                        Ordering::Less => return Ordering::Less,
+                        Ordering::Greater => return Ordering::Greater,
+                        Ordering::Equal => continue,
+                    }
+                }
+                Ordering::Equal
+            }
+        }
     }
 }
 
 impl PartialEq for Game {
     fn eq(&self, other: &Self) -> bool {
-        todo!()
+        self.cmp(&other) == Ordering::Equal
     }
 }
 
 fn puzzle1(data: &str) -> i32 {
-    data.lines().map(|line| {
-        let (hand_str, bid_str) = line.split_once(" ").unwrap();
-        let bid: u32 = bid_str.parse().unwrap();
-        let hand = Hand::from_str(hand_str).unwrap();
-    });
-    0
+    let mut games = data
+        .lines()
+        .map(|line| Game::from_str(line, false))
+        .collect::<Result<Vec<Game>, ParseGameError>>()
+        .unwrap();
+    games.sort();
+    let res: u32 = games
+        .iter()
+        .enumerate()
+        .map(|(i, game)| (i as u32 + 1) * game.bid)
+        .sum();
+    res as i32
 }
 
 fn puzzle2(data: &str) -> i32 {
-    data.len() as i32
+    let mut games = data
+        .lines()
+        .map(|line| Game::from_str(line, true))
+        .collect::<Result<Vec<Game>, ParseGameError>>()
+        .unwrap();
+    games.sort();
+    let res: u32 = games
+        .iter()
+        .enumerate()
+        .map(|(i, game)| (i as u32 + 1) * game.bid)
+        .sum();
+    res as i32
 }
 
 fn main() {
@@ -140,99 +229,267 @@ mod tests {
 
     #[test]
     fn test_puzzle2() {
-        let test_data = "";
+        let test_data = "\
+                         32T3K 765\n\
+                         T55J5 684\n\
+                         KK677 28\n\
+                         KTJJT 220\n\
+                         QQQJA 483";
         let res = puzzle2(test_data);
-        assert_eq!(res, -1);
+        assert_eq!(res, 5905);
     }
 
     #[test]
-    fn test_parse_hand() {
-        assert_eq!(Hand::from_str("AAAAA"), Ok(Hand::FiveOfAKind));
-        assert_eq!(Hand::from_str("88888"), Ok(Hand::FiveOfAKind));
+    fn test_parse_hand_without_joker() {
+        assert_eq!(Hand::from_str("AAAAA", false), Ok(Hand::FiveOfAKind));
+        assert_eq!(Hand::from_str("88888", false), Ok(Hand::FiveOfAKind));
 
-        assert_eq!(Hand::from_str("18888"), Ok(Hand::FourOfAKind));
-        assert_eq!(Hand::from_str("1KKKK"), Ok(Hand::FourOfAKind));
-        assert_eq!(Hand::from_str("KK5KK"), Ok(Hand::FourOfAKind));
+        assert_eq!(Hand::from_str("Q8888", false), Ok(Hand::FourOfAKind));
+        assert_eq!(Hand::from_str("JKKKK", false), Ok(Hand::FourOfAKind));
+        assert_eq!(Hand::from_str("KK5KK", false), Ok(Hand::FourOfAKind));
 
-        assert_eq!(Hand::from_str("K55KK"), Ok(Hand::FullHouse));
-        assert_eq!(Hand::from_str("83838"), Ok(Hand::FullHouse));
+        assert_eq!(Hand::from_str("K55KK", false), Ok(Hand::FullHouse));
+        assert_eq!(Hand::from_str("83838", false), Ok(Hand::FullHouse));
 
-        assert_eq!(Hand::from_str("1K1K4"), Ok(Hand::TwoPair));
-        assert_eq!(Hand::from_str("AA5TT"), Ok(Hand::TwoPair));
+        assert_eq!(Hand::from_str("3K3K4", false), Ok(Hand::TwoPair));
+        assert_eq!(Hand::from_str("AA5TT", false), Ok(Hand::TwoPair));
 
-        assert_eq!(Hand::from_str("AA54T"), Ok(Hand::OnePair));
-        assert_eq!(Hand::from_str("T354T"), Ok(Hand::OnePair));
+        assert_eq!(Hand::from_str("AA54T", false), Ok(Hand::OnePair));
+        assert_eq!(Hand::from_str("T354T", false), Ok(Hand::OnePair));
 
-        assert_eq!(Hand::from_str("12345"), Ok(Hand::HighCard));
+        assert_eq!(Hand::from_str("23456", false), Ok(Hand::HighCard));
+
+        // Parsing error
+        assert_eq!(Hand::from_str("2345", false), Err(ParseHandError));
+        assert_eq!(Hand::from_str("234567", false), Err(ParseHandError));
+    }
+
+    #[test]
+    fn test_parse_hand_with_joker() {
+        assert_eq!(Hand::from_str("AAAAA", true), Ok(Hand::FiveOfAKind));
+        assert_eq!(Hand::from_str("88888", true), Ok(Hand::FiveOfAKind));
+        assert_eq!(Hand::from_str("JKKKK", true), Ok(Hand::FiveOfAKind));
+        assert_eq!(Hand::from_str("JJJJJ", true), Ok(Hand::FiveOfAKind));
+
+        assert_eq!(Hand::from_str("Q8888", true), Ok(Hand::FourOfAKind));
+        assert_eq!(Hand::from_str("KK5KK", true), Ok(Hand::FourOfAKind));
+        assert_eq!(Hand::from_str("KK5JK", true), Ok(Hand::FourOfAKind));
+        assert_eq!(Hand::from_str("838J8", true), Ok(Hand::FourOfAKind));
+
+        assert_eq!(Hand::from_str("K55KK", true), Ok(Hand::FullHouse));
+        assert_eq!(Hand::from_str("83838", true), Ok(Hand::FullHouse));
+        assert_eq!(Hand::from_str("8383J", true), Ok(Hand::FullHouse));
+
+        assert_eq!(Hand::from_str("AA5AT", true), Ok(Hand::ThreeOfAKind));
+        assert_eq!(Hand::from_str("AA5JT", true), Ok(Hand::ThreeOfAKind));
+        assert_eq!(Hand::from_str("3KJK4", true), Ok(Hand::ThreeOfAKind));
+
+        assert_eq!(Hand::from_str("3K3K4", true), Ok(Hand::TwoPair));
+        assert_eq!(Hand::from_str("AA5TT", true), Ok(Hand::TwoPair));
+
+        assert_eq!(Hand::from_str("JA54T", true), Ok(Hand::OnePair));
+        assert_eq!(Hand::from_str("J354T", true), Ok(Hand::OnePair));
+
+        assert_eq!(Hand::from_str("23456", true), Ok(Hand::HighCard));
+
+        // Parsing error
+        assert_eq!(Hand::from_str("2345", true), Err(ParseHandError));
+        assert_eq!(Hand::from_str("234567", true), Err(ParseHandError));
     }
 
     #[test]
     fn test_parse_game() {
         assert_eq!(
-            Game::from_str("AAAAA 123"),
+            Game::from_str("AAAAA 123", false),
             Ok(Game {
                 hand: Hand::FiveOfAKind,
                 hand_str: "AAAAA".to_owned(),
-                bid: 123
+                bid: 123,
+                with_joker: false
             })
         );
         assert_eq!(
-            Game::from_str("88888 234"),
+            Game::from_str("88888 234", false),
             Ok(Game {
                 hand: Hand::FiveOfAKind,
                 hand_str: "88888".to_owned(),
-                bid: 234
+                bid: 234,
+                with_joker: false
             })
         );
 
         assert_eq!(
-            Game::from_str("18888 3"),
+            Game::from_str("A8888 3", false),
             Ok(Game {
                 hand: Hand::FourOfAKind,
-                hand_str: "18888".to_owned(),
-                bid: 3
+                hand_str: "A8888".to_owned(),
+                bid: 3,
+                with_joker: false
             })
         );
         assert_eq!(
-            Game::from_str("1KKKK 4567"),
+            Game::from_str("TKKKK 4567", false),
             Ok(Game {
                 hand: Hand::FourOfAKind,
-                hand_str: "1KKKK".to_owned(),
-                bid: 4567
+                hand_str: "TKKKK".to_owned(),
+                bid: 4567,
+                with_joker: false
             })
         );
 
         assert_eq!(
-            Game::from_str("K55KK 34"),
+            Game::from_str("K55KK 34", false),
             Ok(Game {
                 hand: Hand::FullHouse,
                 hand_str: "K55KK".to_owned(),
-                bid: 34
+                bid: 34,
+                with_joker: false
             })
         );
         assert_eq!(
-            Game::from_str("1K1K4 24"),
+            Game::from_str("2Q2Q4 24", false),
             Ok(Game {
                 hand: Hand::TwoPair,
-                hand_str: "1K1K4".to_owned(),
-                bid: 24
+                hand_str: "2Q2Q4".to_owned(),
+                bid: 24,
+                with_joker: false
             })
         );
         assert_eq!(
-            Game::from_str("AA54T 2"),
+            Game::from_str("JJ54T 2", false),
             Ok(Game {
                 hand: Hand::OnePair,
-                hand_str: "AA54T".to_owned(),
-                bid: 2
+                hand_str: "JJ54T".to_owned(),
+                bid: 2,
+                with_joker: false
             })
         );
         assert_eq!(
-            Game::from_str("12345 93"),
+            Game::from_str("23456 93", false),
             Ok(Game {
                 hand: Hand::HighCard,
-                hand_str: "12345".to_owned(),
-                bid: 93
+                hand_str: "23456".to_owned(),
+                bid: 93,
+                with_joker: false
             })
         );
+    }
+
+    #[test]
+    fn test_game_ordering_without_joker() {
+        let game_higher = Game {
+            hand: Hand::OnePair,
+            hand_str: "AA54T".to_owned(),
+            bid: 2,
+            with_joker: false,
+        };
+        let game_lower = Game {
+            hand: Hand::HighCard,
+            hand_str: "23456".to_owned(),
+            bid: 93,
+            with_joker: false,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
+
+        let game_higher = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "AAAAA".to_owned(),
+            bid: 123,
+            with_joker: false,
+        };
+        let game_lower = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "88888".to_owned(),
+            bid: 234,
+            with_joker: false,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
+
+        let game_higher = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "AAAAA".to_owned(),
+            bid: 123,
+            with_joker: false,
+        };
+        let game_lower = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "JJJJJ".to_owned(),
+            bid: 234,
+            with_joker: false,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
+    }
+
+    #[test]
+    fn test_game_ordering_with_joker() {
+        let game_higher = Game {
+            hand: Hand::OnePair,
+            hand_str: "AA54T".to_owned(),
+            bid: 2,
+            with_joker: true,
+        };
+        let game_lower = Game {
+            hand: Hand::HighCard,
+            hand_str: "23456".to_owned(),
+            bid: 93,
+            with_joker: true,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
+
+        let game_higher = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "AAAAA".to_owned(),
+            bid: 123,
+            with_joker: true,
+        };
+        let game_lower = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "88888".to_owned(),
+            bid: 234,
+            with_joker: true,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
+
+        let game_higher = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "JJAJJ".to_owned(),
+            bid: 123,
+            with_joker: true,
+        };
+        let game_lower = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "JJJAA".to_owned(),
+            bid: 234,
+            with_joker: true,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
+
+        let game_higher = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "22222".to_owned(),
+            bid: 123,
+            with_joker: true,
+        };
+        let game_lower = Game {
+            hand: Hand::FiveOfAKind,
+            hand_str: "JJJJJ".to_owned(),
+            bid: 234,
+            with_joker: true,
+        };
+        assert!(game_higher != game_lower);
+        assert!(game_higher > game_lower);
+        assert!(game_higher >= game_lower);
     }
 }
