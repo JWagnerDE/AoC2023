fce009f147e1ea2a2810a1a139a455821d835d9e
Jannis Wagner <jannis.wagner.de@gmail.com>
2023-12-16 20:26:56 +0100

    First implementation. But it takes very long

--- rust/src/bin/day8.rs
+++ rust/src/bin/day8.rs
@@ -2,9 +2,44 @@
 use std::env;
 use std::fs;
 
+use std::collections::HashMap;
+
 
 fn puzzle1(data: &str) -> i32 {
-    data.len() as i32
+    let (instructions, documents_str) = data.split_once("\n\n").unwrap();
+    let mut document_map = HashMap::new();
+    for line in documents_str.split("\n").filter(|line| !line.is_empty()) {
+        let (key, right_side) = line.split_once(" = ").unwrap();
+        let (left_node, right_node) = right_side
+            .trim_start_matches("(")
+            .trim_end_matches(")")
+            .split_once(", ")
+            .unwrap();
+        document_map.insert(key, (left_node, right_node));
+    }
+    let mut steps: u32 = 0;
+    let mut current = documents_str.split_whitespace().next().unwrap();
+    // TODO: Since we need to cycle the instructions untwill we reach ZZZ,
+    //       This operation will take very long with the real dataset.
+    //       A better soloution would be to precompute for every document
+    //       The next document if we get R or L as input.
+    //       Even better would be to make the computation have 2 3 or 4
+    //       instructions.
+    //       So compute the next document if we have RRLR on doc ABC.
+    //       But think of the exit criteria.
+    //       Especially if it is in the middle of the precomputed step.
+    for dir in instructions.chars().cycle() {
+        current = match dir {
+            'L' => document_map.get(current).unwrap().0,
+            'R' => document_map.get(current).unwrap().1,
+            _ => unreachable!()
+        };
+        steps += 1;
+        if current == "ZZZ" {
+            break
+        }
+    }
+    steps as i32
 }
 
 fn puzzle2(data: &str) -> i32 {
@@ -32,12 +67,31 @@ mod tests {
 
     #[test]
     fn test_puzzle1() {
-        let test_data = "";
+        let test_data = "\
+                         RL\n\
+                         \n\
+                         AAA = (BBB, CCC)\n\
+                         BBB = (DDD, EEE)\n\
+                         CCC = (ZZZ, GGG)\n\
+                         DDD = (DDD, DDD)\n\
+                         EEE = (EEE, EEE)\n\
+                         GGG = (GGG, GGG)\n\
+                         ZZZ = (ZZZ, ZZZ)";
         let res = puzzle1(test_data);
-        assert_eq!(res, -1);
+        assert_eq!(res, 2);
+
+        let test_data = "\
+                         LLR\n\
+                         \n\
+                         AAA = (BBB, BBB)\n\
+                         BBB = (AAA, ZZZ)\n\
+                         ZZZ = (ZZZ, ZZZ)";
+        let res = puzzle1(test_data);
+        assert_eq!(res, 6);
     }
 
     #[test]
+    #[ignore]
     fn test_puzzle2() {
         let test_data = "";
         let res = puzzle2(test_data);
